c9da9119f339362413909235f5c246ac7f8e4951
ikawamuk <ikawamuk@student.42.fr>
2025-04-26 08:43:38 +0900

    memcpy test

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 11:42:29 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/04/25 15:45:12 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/04/26 08:43:36 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
-	char	*dp;
-	char	*sp;
+	unsigned char	*dp;
+	unsigned char	*sp;
 
-	dp = (char *)dest;
-	sp = (char *)src;
+	dp = (unsigned char *)dest;
+	sp = (unsigned char *)src;
 	i = 0;
 	while (i < n)
 	{
@@ -28,3 +28,32 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	}
 	return (dest);
 }
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	unsigned char	src[] = {'a', 'b', 'c', 'A', '_',0x00,  0x88, 0xff,};
+	int		n = sizeof(src);
+	int		size = 20;
+	unsigned char	dest[size];
+	int		i = 0;
+
+	ft_bzero(dest, size);
+	ft_memcpy(dest, src, n);
+	
+	printf("src:    ");
+	i = 0;
+	while (i < size)
+		printf("%02x ", src[i++]);
+	printf("\n");
+	
+	printf("after:  ");
+	i = 0;
+	while (i < size)
+		printf("%02x ", dest[i++]);
+	printf("\n");
+	return 0;
+}
+*/
