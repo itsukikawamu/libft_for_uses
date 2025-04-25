e246634992ac5b02d1617a0109318250aff3f633
ikawamuk <ikawamuk@student.42.fr>
2025-04-26 07:31:16 +0900

    isalpha test

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 04:31:25 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/04/25 10:59:12 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/04/26 07:30:58 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,3 +18,17 @@ int	ft_isalpha(int c)
 		return (FT_ISALPHA);
 	return (0);
 }
+
+/*
+#include <stdio.h>
+int main(int argc, char* argv[])
+{
+	if(argc != 2)
+		return (1);
+	if (ft_isalpha(argv[1][0]))
+		printf("it is alpha.\n");
+	else
+		printf("it isn't alpha.\n");
+	return (0);
+}
+*/
