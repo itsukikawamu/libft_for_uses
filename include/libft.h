69dd54e619a512485b225c0ef248cb28bc943845
ikawamuk <ikawamuk@student.42.fr>
2025-04-25 11:22:20 +0900

    ispucnct

--- include/libft.h
+++ include/libft.h
@@ -6,19 +6,22 @@
 /*   By: ikawamuk <ikawamuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 09:51:09 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/04/25 10:58:53 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/04/25 11:21:26 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-#define FT_ISSUPPER 0x01
-#define FT_ISLOWER 0x02
-#define FT_ISALPHA 0x20
-#define FT_ISDIGIT 0x30
-#define FT_ISALNUM 0x04
-#define FT_ISASCII 0x01
-#define FT_ISPRINT 0x07
+#define FT_ISSUPPER	0x0001
+#define FT_ISLOWER	0x0002
+#define FT_ISALPHA	0x0004
+#define FT_ISDIGIT	0x0008
+#define FT_XDIGIT	0x0010
+
+#define FT_ISALNUM	0x0000
+#define FT_ISASCII	0x0000
+#define FT_ISPRINT	0x0040
+#define FT_ISPUNCT	0x8000
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
