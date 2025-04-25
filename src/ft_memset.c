b302b3c85bcae35d6e9602e5bc8138d0af4dd4e8
ikawamuk <ikawamuk@student.42.fr>
2025-04-25 10:23:48 +0900

    memset

---
+++ src/ft_memset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikawamuk <ikawamuk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/25 10:08:19 by ikawamuk          #+#    #+#             */
+/*   Updated: 2025/04/25 10:23:24 by ikawamuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, unsigned int n)
+{
+	unsigned int	i;
+	unsigned char	*p;
+
+	p = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		p[i] = c;
+		i++;
+	}
+	return (s);
+}
--- src/ft_strlen.c
+++ src/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 10:02:06 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/04/25 10:08:06 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/04/25 10:11:36 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ unsigned int	ft_strlen(const char *s)
 {
 	unsigned int	len;
 
+	len = 0;
 	while (s[len] != '\0')
 		len++;
 	return (len);
