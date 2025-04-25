d843ab6be251cf6f3f4522f89d6bbe7cee9aa6d1
ikawamuk <ikawamuk@student.42.fr>
2025-04-25 09:43:06 +0900

    I didn't need care about actual value of "not zero"

--- src/ft_isalpha.c
+++ src/ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 04:31:25 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/04/25 07:26:51 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/04/25 09:42:12 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,5 @@
 
 int	ft_isalpha(int c)
 {
-	if (('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z'))
-		return (ft_ISalpha);
-	return (0);
-}
-
-#include <ctype.h>
-#include <stdio.h>
-#include <assert.h>
-int main(int argc, char *argv[])
-{
-	if (argc != 2)
-		return (1);
-	char c = argv[1][0];
-	printf("%d\n", ft_isalpha(c));
-	printf("%d\n", isalpha(c));
+	return (('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z'));
 }
