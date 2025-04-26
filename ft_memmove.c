9d5e28756ebdbf4468932a35804e938bf71eb4cd
ikawamuk <ikawamuk@student.42.fr>
2025-04-26 09:43:50 +0900

    test memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 12:03:07 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/04/26 08:43:42 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/04/26 09:43:34 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,46 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char	*dp;
-	char	*tmp;
+	unsigned char		*d;
+	const unsigned char	*s;
 
-	dp = (char *)dest;
-	tmp = ft_memcpy(tmp, src, n);
-	dest = ft_memcopy(dest, src, n);
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	if (d < s)
+		ft_memcpy(d, s, n);
+	else
+	{
+		while (0 < n--)
+			d[n] = s[n];
+	}
 	return (dest);
 }
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	unsigned char	src[] = {'a', 'b', 'c', 'A', '_',0x00,  0x88, 0xff,};
+	int		n = sizeof(src);
+	int		size = 20;
+	unsigned char	dest[size];
+	int		i = 0;
+
+	ft_memset(dest, 0xff, size);
+	ft_memmove(dest, src, n);
+	
+	printf("src:    ");
+	i = 0;
+	while (i < size)
+		printf("%02x ", src[i++]);
+	printf("\n");
+	
+	printf("after:  ");
+	i = 0;
+	while (i < size)
+		printf("%02x ", dest[i++]);
+	printf("\n");
+	return 0;
+}
+*/
