629cca588e9e9802da86f349f63bedc5bdb3eb07
ikawamuk <ikawamuk@student.42.fr>
2025-04-26 09:56:34 +0900

    test strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 10:02:06 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/04/25 14:39:07 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/04/26 09:56:25 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,13 @@ size_t	ft_strlen(const char *s)
 		len++;
 	return (len);
 }
+
+/*
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	if (argc!=2)
+		return (1);
+	printf("%zu\n", ft_strlen(argv[1]));
+}
+*/
