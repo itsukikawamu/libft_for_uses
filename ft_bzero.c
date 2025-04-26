afe5ad7c81bf0dcb6b399fee09735eba8a48bf95
ikawamuk <ikawamuk@student.42.fr>
2025-04-26 09:00:21 +0900

    test memset

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 10:28:57 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/04/26 07:12:10 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/04/26 09:00:10 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void	ft_bzero(void *s, size_t n)
 #include <stdlib.h>
 int main(int argc, char *argv[])
 {
-	if (argc < 2)
+	if (argc != 2)
 		return (1);
 	size_t size = atoi(argv[1]);
 	unsigned char *ptr = malloc(size);
@@ -41,13 +41,13 @@ int main(int argc, char *argv[])
 	while (i < size)
 	{
 		ptr[i] = i * 7 + 9;
-		printf("%02x", ptr[i++]);
+		printf("%02x ", ptr[i++]);
 	}
 	printf("%c", '\n');
 	ft_bzero(ptr, size);
 	i = 0;
 	while (i < size)
-		printf("%02x", ptr[i++]);
+		printf("%02x ", ptr[i++]);
 	free(ptr);
 	return (0);
 }
--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 10:08:19 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/04/25 10:43:39 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/04/26 08:59:23 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,3 +26,31 @@ void	*ft_memset(void *s, int c, size_t n)
 	}
 	return (s);
 }
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+		return (1);
+	size_t size = atoi(argv[1]);
+	char c = argv[2][0];
+	unsigned char *ptr = malloc(size);
+	if (!ptr)
+		return (1);
+	size_t i = 0;
+	while (i < size)
+	{
+		ptr[i] = i * 7 + 9;
+		printf("%02x ", ptr[i++]);
+	}
+	printf("%c", '\n');
+	ft_memset(ptr, (int)c, size);
+	i = 0;
+	while (i < size)
+		printf("%02x ", ptr[i++]);
+	free(ptr);
+	return (0);
+}
+*/
