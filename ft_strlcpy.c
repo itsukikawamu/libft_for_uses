a4e811431f79d243fce81e65c2d826f76135ff73
ikawamuk <ikawamuk@student.42.fr>
2025-04-26 10:12:44 +0900

    test strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 14:28:40 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/04/25 15:27:56 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/04/26 10:11:28 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,3 +29,21 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	dst[i] = '\0';
 	return (len);
 }
+
+/*
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+		return (1);
+	int		size = 10;
+	char	dst[size];
+
+
+	size_t	reval = ft_strlcpy(dst, argv[1], size);
+	printf("src: %s\n", argv[1]);
+	printf("dst: %s\n", dst);
+	printf("return value: %zu\n", reval);
+	return 0;
+}
+*/
