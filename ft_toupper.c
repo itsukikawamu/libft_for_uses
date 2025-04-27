d4794c98f6cf1d273f1924d42d735982192c88f9
ikawamuk <ikawamuk@student.42.fr>
2025-04-27 13:27:35 +0900

    toupper

---
+++ ft_toupper.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikawamuk <ikawamuk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/27 13:20:06 by ikawamuk          #+#    #+#             */
+/*   Updated: 2025/04/27 13:27:25 by ikawamuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_toupper(int c)
+{
+	if (ft_islower(c))
+		c += ('A' - 'a');
+	return c;
+}
+
+int	ft_islower(int c)
+{
+	if ('a' <= c && c <= 'z')
+		return (FT_ISLOWER);
+	return (0);
+}
+
+/*
+#include <stdio.h>
+#include <assert.h>
+#include <limits.h>
+int main(void)
+{
+    assert(ft_toupper('a') == 'A');
+    assert(ft_toupper('z') == 'Z');
+    assert(ft_toupper('m') == 'M');
+    assert(ft_toupper('A') == 'A');
+    assert(ft_toupper('Z') == 'Z');
+    assert(ft_toupper('0') == '0');
+    assert(ft_toupper('9') == '9');
+    assert(ft_toupper('!') == '!');
+    assert(ft_toupper('@') == '@');
+    assert(ft_toupper(' ') == ' ');
+    assert(ft_toupper(0) == 0);
+    assert(ft_toupper(127) == 127);
+
+    printf("all test passed\n");
+    return (0);
+}
+*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 09:51:09 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/04/26 14:57:38 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/04/27 13:26:27 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,5 +42,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int		ft_toupper(int c);
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
