ed7e7da737c08461da7411109efd227684cd1bfb
ikawamuk <ikawamuk@student.42.fr>
2025-04-27 15:00:23 +0900

    update

---
+++ ft_strchr.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikawamuk <ikawamuk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/27 13:39:55 by ikawamuk          #+#    #+#             */
+/*   Updated: 2025/04/27 14:59:16 by ikawamuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	int i;
+	int	len;
+
+	len = ft_strlen(s);
+	i = 0;
+	while (i <= len)
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+
+#include <stdio.h>
+#include <assert.h>
+int main(void)
+{
+	const char *str = "hello";
+    assert(ft_strchr(str, 'e') == str + 1); 
+    assert(ft_strchr(str, 'l') == str + 2);
+    assert(ft_strchr(str, 'o') == str + 4);
+    assert(ft_strchr(str, 'x') == NULL);
+    assert(ft_strchr(str, '\0') == str + 5);
+    assert(ft_strchr("", 'a') == NULL);
+
+    printf("All tests passed!\n");
+}
