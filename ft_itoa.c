e91b1f525d82bbd0bc1a622085d85e5ec6ab2673
ikawamuk <ikawamuk@student.42.fr>
2025-04-29 00:21:18 +0900

    itoa

---
+++ ft_itoa.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikawamuk <ikawamuk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/28 22:42:13 by ikawamuk          #+#    #+#             */
+/*   Updated: 2025/04/29 00:21:06 by ikawamuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static void		make_numstr(int n, char *arr, size_t size);
+static size_t	count_digit(int n);
+
+char	*ft_itoa(int n)
+{
+	char	*res;
+	size_t	digit;
+
+	digit = count_digit(n);
+	res = ft_calloc(digit + 1, sizeof(char));
+	if (!res)
+		return (NULL);
+	if (n == 0)
+	{
+		*res = '0';
+		return (res);
+	}
+	make_numstr(n, res, digit);
+	return (res);
+}
+
+static void	make_numstr(int nbr, char *arr, size_t size)
+{
+	int	tmp;
+	int	n;
+
+	arr[size--] = '\0';
+	n = nbr;
+	while (n != 0)
+	{
+		tmp = n % 10;
+		if (tmp < 0)
+			tmp = -tmp;
+		arr[size] = '0' + tmp;
+		n /= 10;
+		size--;
+	}
+	if (nbr < 0)
+		arr[size] = '-';
+}
+
+static size_t	count_digit(int n)
+{
+	size_t	digit;
+
+	if (n == 0)
+		return (1);
+	digit = 0;
+	if (n < 0)
+		digit++;
+	while (n != 0)
+	{
+		n /= 10;
+		digit++;
+	}
+	return (digit);
+}
+
+/*
+int main(void)
+{
+	printf("%s\n", ft_itoa(0));
+	printf("%s\n", ft_itoa(-200));
+	return(0);
+}
+*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 22:24:06 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/04/28 22:24:17 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/04/28 22:43:12 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,5 +73,6 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strndup(const char *s, size_t len);
 char	*ft_strncpy(char *dst, const char *src, size_t size);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 
 #endif
