f009cb7567b16dd54ccd8a1ae2639788fdb6355f
ikawamuk <ikawamuk@student.42.fr>
2025-04-29 10:55:50 +0900

    test lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 10:07:17 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/04/29 10:26:49 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/04/29 10:55:01 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ t_list	*ft_lstnew(void *content)
 	t_list	*res;
 
 	res = malloc(sizeof(t_list));
+	if (!res)
+		return (NULL);
 	res->content = content;
 	res->next = NULL;
 	return (res);
