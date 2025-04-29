1981fb58d3af3622659064592370886920056b09
ikawamuk <ikawamuk@student.42.fr>
2025-04-29 21:50:03 +0900

    ok

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ikawamuk <ikawamuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 11:27:06 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/04/29 16:58:45 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/04/29 21:48:48 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,4 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	while ((last)->next)
 		last = (last)->next;
 	(last)->next = new;
-	new->next = NULL;
 }
